876c3d2002f2dd950030fc442ffc8f8a3d931046
Tony Adams <anto-kha2@yandex.ru>
2018-07-15 15:42:49 +0300

    DEBUG False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%lefec67ek9em@xik_1n(q!gi$)@k+5%9qoy($z886v0hecie%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'adamsLD.pythonanywhere.com']
 
